a4b3d8e6ff68f32e0e890d982a3b364618c51b04
Roberto Armas <roberto.armas@verndale.com>
2023-05-22 20:24:47 -0500

    type bug fix

--- src/foundation-mapper/src/cast-item.ts
+++ src/foundation-mapper/src/cast-item.ts
@@ -1,5 +1,5 @@
-import { Item } from '@sitecore-jss/sitecore-jss-nextjs';
+import { ComponentRendering, Item } from '@sitecore-jss/sitecore-jss-nextjs';
 
-export function castItem<T>(item: Item): T | null {
+export function castItem<T>(item: Item | ComponentRendering): T | null {
   return item.fields as T;
 }
