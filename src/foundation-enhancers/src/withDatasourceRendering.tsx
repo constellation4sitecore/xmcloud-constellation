593d5566b61171059352af5b8c57c20e65e41b16
Roberto Armas <roberto.armas@verndale.com>
2023-09-12 19:45:15 -0500

    try with different inject method

--- src/foundation-enhancers/src/withDatasourceRendering.tsx
+++ src/foundation-enhancers/src/withDatasourceRendering.tsx
@@ -31,7 +31,7 @@ export function withDatasourceRendering(){
         );
       }
       
-      return React.createElement(InjectedComponent, props);
+      return <InjectedComponent {...props} />;
     }
   }
 }
