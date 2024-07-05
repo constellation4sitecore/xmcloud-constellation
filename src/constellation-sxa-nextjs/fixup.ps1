d6131ab795ec2ae1506183c5fa0dbe82cf33262c
Roberto Armas <roberto.armas@verndale.com>
2024-07-05 13:08:20 -0500

    fix script for windows build & fix types

---
+++ src/constellation-sxa-nextjs/fixup.ps1
@@ -0,0 +1,15 @@
+$commonJsContent = @'
+{
+    "type": "commonjs"
+}
+'@
+
+$moduleContent = @'
+{
+    "type": "module"
+}
+'@
+
+# Write the content to the files
+$commonJsContent | Out-File -FilePath "dist/cjs/package.json" -Force
+$moduleContent | Out-File -FilePath "dist/mjs/package.json" -Force
