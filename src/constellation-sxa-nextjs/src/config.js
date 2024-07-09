5fd759e0bf03bc4386f5a1d85b018276518430ba
Roberto Armas <roberto.armas@verndale.com>
2024-07-09 09:23:57 -0500

    change to cjs extension

--- src/constellation-sxa-nextjs/src/config.js
+++ src/constellation-sxa-nextjs/src/config.js
@@ -1,4 +1,4 @@
 // Code: workaraound to inject project settings into 
-import jssConfig from '../../../../../src/temp/config.js';
+import jssConfig from '../../../../../src/temp/config.cjs';
 const config = jssConfig;
 export default config;
