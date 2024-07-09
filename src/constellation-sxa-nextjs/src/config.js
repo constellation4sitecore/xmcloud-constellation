9eb9bce75a4ee014c891e1434b90cb74d8f145b1
Roberto Armas <roberto.armas@verndale.com>
2024-07-08 23:10:36 -0500

    update import reference

--- src/constellation-sxa-nextjs/src/config.js
+++ src/constellation-sxa-nextjs/src/config.js
@@ -1,4 +1,4 @@
 // Code: workaraound to inject project settings into 
-import jssConfig from '../../../../../src/temp/config';
+import jssConfig from '../../../../../src/temp/config.js';
 const config = jssConfig;
 export default config;
