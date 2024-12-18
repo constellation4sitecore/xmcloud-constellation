e368f1faf629ff716193051438ba1ac8cf008e6d
Roberto Armas <roberto.armas@verndale.com>
2024-12-18 17:17:37 -0500

    fix null error

--- src/page-analytics-scripts/src/helpers/index.ts
+++ src/page-analytics-scripts/src/helpers/index.ts
@@ -14,7 +14,10 @@ export const extractAnalyticsProps = (props: ExtendedDocumentInitialProps) => {
   if (props.__NEXT_DATA__.props.pageProps.statusCode == 500) return null;
   const analyticsProps = props.__NEXT_DATA__.props.pageProps.componentProps[
     'analyticScripts'
-  ] as AnalyticScripts;
-  console.log(analyticsProps);
+  ] as AnalyticScripts & { error?: string };
+  if (analyticsProps?.error) {
+    console.error(analyticsProps.error);
+    return null;
+  }
   return analyticsProps;
 };
