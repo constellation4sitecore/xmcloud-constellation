1f6fbdcad1d16c5a4d6255ed491374bdeccb03ca
Roberto Armas <roberto.armas@verndale.com>
2025-01-28 00:49:51 -0500

    constellation upgrade

--- src/enhancers/src/withDatasourceRendering.tsx
+++ src/enhancers/src/withDatasourceRendering.tsx
@@ -1,5 +1,5 @@
 import { ComponentRendering, useSitecoreContext } from '@sitecore-jss/sitecore-jss-nextjs';
-import React from 'react';
+import React, { ComponentType, ReactElement } from 'react';
 
 export interface WithDatasourceRenderingProps {
   rendering: ComponentRendering;
@@ -7,23 +7,22 @@ export interface WithDatasourceRenderingProps {
   dataFields?: unknown;
 }
 
-export function withDatasourceRendering(){
+export function withDatasourceRendering() {
   return function withDatasourceRenderingHoc<ComponentProps extends WithDatasourceRenderingProps>(
-    componentOrMap: React.ComponentType<ComponentProps> | { [key: string]: React.ComponentType<ComponentProps> }
-  ){
-    return function WithDatasourceRendering(props: ComponentProps)
-    {
+    componentOrMap: ComponentType<ComponentProps> | { [key: string]: ComponentType<ComponentProps> }
+  ) {
+    return function WithDatasourceRendering(props: ComponentProps): ReactElement {
       const { sitecoreContext } = useSitecoreContext();
       const siteName: string = sitecoreContext.site?.name ?? 'default';
-  
-      let InjectedComponent: React.ComponentType<ComponentProps>;
-  
+
+      let InjectedComponent: ComponentType<ComponentProps>;
+
       if (typeof componentOrMap === 'function') {
         InjectedComponent = componentOrMap;
       } else {
         InjectedComponent = componentOrMap[siteName] ?? componentOrMap['default'];
       }
-  
+
       if (!props.rendering.dataSource) {
         return React.createElement(
           InjectedComponent,
@@ -31,8 +30,7 @@ export function withDatasourceRendering(){
         );
       }
 
-      return <InjectedComponent {...props} />;
-    }
-  }
+      return React.createElement(InjectedComponent, props);
+    };
+  };
 }
-
