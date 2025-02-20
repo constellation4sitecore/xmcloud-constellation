2e79fce09a2d67ce51a4c6b642085ede0f1c7ab5
HQ\Richard.Leiva <richard.leiva@verndale.com>
2025-02-20 12:15:20 -0500

    Add export for the ImageNavigationLink so it can be referenced externally.

--- src/navigation/src/models/index.ts
+++ src/navigation/src/models/index.ts
@@ -1,4 +1,5 @@
 export { NavigationLink } from './navigation-link';
+export { ImageNavigationLink } from './image-navigation-link';
 export { LinkFields } from './link-fields';
 export { NavigationMenu } from './navigation-menu';
 export { TargetItem } from './target-item';
