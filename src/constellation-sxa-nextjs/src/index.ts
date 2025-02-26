643a4f3968fdb8bf9d711432aa8332b3fd2c0d1e
Roberto Armas <roberto.armas@verndale.com>
2025-02-26 16:13:25 -0500

    add hasContent helper

--- src/constellation-sxa-nextjs/src/index.ts
+++ src/constellation-sxa-nextjs/src/index.ts
@@ -3,3 +3,4 @@ import config, { JssConfig } from './config';
 export { config, JssConfig };
 export * from './fields-extensions';
 export * from './graphql-client-factory';
+export * from './helpers';
