fd5ac6243cddb2cbb2610af3fe2fcacd0c50a35a
Roberto Armas <roberto.armas@verndale.com>
2025-02-26 22:44:00 -0500

    fix react key loop issue

--- src/page-analytics-scripts/src/components/PageAnalyticsNextScripts.tsx
+++ src/page-analytics-scripts/src/components/PageAnalyticsNextScripts.tsx
@@ -6,48 +6,42 @@ import Script from 'next/script';
 
 const PageAnalyticsNextScripts = (props: AnalyticsScriptsProps): JSX.Element => (
   <>
-    {props.scripts.map((script: AnalyticScriptItem) => {
+    {props.scripts.map((script: AnalyticScriptItem, index: number) => {
       if (script.template.id === TEMPLATES_ID.contentScript) {
         const contentScriptModel = mapToNew<ContentScript>(script);
         const computedStrategy = contentScriptModel?.lazy?.value ? 'lazyOnload' : props.strategy;
         return (
-          <>
-            {contentScriptModel && (
-              <>
-                {!contentScriptModel.noScript.value && (
-                  <Script
-                    key={script.id}
-                    id={`_next-script-${script.id}-init`}
-                    strategy={computedStrategy}
-                    dangerouslySetInnerHTML={{ __html: contentScriptModel.contentScript.value }}
-                  />
-                )}
-                {contentScriptModel.noScript.value && (
-                  <noscript
-                    key={script.id}
-                    dangerouslySetInnerHTML={{ __html: contentScriptModel.contentScript.value }}
-                  />
-                )}
-              </>
-            )}
-          </>
+          contentScriptModel && (
+            <React.Fragment key={`${script.id}-${index}`}>
+              {!contentScriptModel.noScript.value && (
+                <Script
+                  id={`_next-script-${script.id}-init`}
+                  strategy={computedStrategy}
+                  dangerouslySetInnerHTML={{ __html: contentScriptModel.contentScript.value }}
+                />
+              )}
+              {contentScriptModel.noScript.value && (
+                <noscript
+                  dangerouslySetInnerHTML={{ __html: contentScriptModel.contentScript.value }}
+                />
+              )}
+            </React.Fragment>
+          )
         );
       } else {
         const mappedScriptModel = mapToNew<UrlScript>(script);
         const computedStrategy = mappedScriptModel?.lazy?.value ? 'lazyOnload' : props.strategy;
         return (
-          <>
-            {mappedScriptModel && (
-              <Script
-                key={script.id}
-                id={`_next-script-${script.id}-init`}
-                strategy={computedStrategy}
-                src={mappedScriptModel.urlScript.value.href}
-                async={mappedScriptModel?.async?.value ? true : undefined}
-                defer={mappedScriptModel?.defer?.value ? true : undefined}
-              />
-            )}
-          </>
+          mappedScriptModel && (
+            <Script
+              key={`${script.id}-${index}`}
+              id={`_next-script-${script.id}-init`}
+              strategy={computedStrategy}
+              src={mappedScriptModel.urlScript.value.href}
+              async={mappedScriptModel?.async?.value ? true : undefined}
+              defer={mappedScriptModel?.defer?.value ? true : undefined}
+            />
+          )
         );
       }
     })}
