fda23447cf43858ef135dc904f472a2778ad41ef
Roberto Armas <roberto.armas@verndale.com>
2025-02-26 23:27:31 -0500

    remove lazy support for Page Analytics Scripts

--- src/page-analytics-scripts/src/components/PageAnalyticsScripts.tsx
+++ src/page-analytics-scripts/src/components/PageAnalyticsScripts.tsx
@@ -2,61 +2,54 @@ import { mapToNew } from '@constellation4sitecore/mapper';
 import React from 'react';
 import { AnalyticScriptItem, AnalyticsScriptsProps, ContentScript, UrlScript } from '../models';
 import { TEMPLATES_ID } from '../constants/analyticsScripts';
-import { isServer } from '@constellation4sitecore/constellation-sxa-nextjs';
 
-const PageAnalyticsScripts = (props: AnalyticsScriptsProps): JSX.Element => {
-  return (
-    <>
-      {props.scripts.map((script: AnalyticScriptItem) => {
-        if (script.template.id === TEMPLATES_ID.contentScript) {
-          const contentScriptModel = mapToNew<ContentScript>(script);
-          const computedStrategy = contentScriptModel?.lazy?.value ? 'lazyOnload' : props.strategy;
-          if (computedStrategy === 'lazyOnload' && isServer()) return null;
-          return (
-            <>
-              {contentScriptModel && (
-                <>
-                  {!contentScriptModel.noScript.value && (
-                    <script
-                      key={script.id}
-                      id={`_next-${script.id}-init`}
-                      type="text/javascript"
-                      data-nscript={computedStrategy}
-                      dangerouslySetInnerHTML={{ __html: contentScriptModel.contentScript.value }}
-                    />
-                  )}
-                  {contentScriptModel.noScript.value && (
-                    <noscript
-                      key={script.id}
-                      dangerouslySetInnerHTML={{ __html: contentScriptModel.contentScript.value }}
-                    />
-                  )}
-                </>
-              )}
-            </>
-          );
-        } else {
-          const mappedScriptModel = mapToNew<UrlScript>(script);
-          const computedStrategy = mappedScriptModel?.lazy?.value ? 'lazyOnload' : props.strategy;
-          if (computedStrategy === 'lazyOnload' && isServer()) return null;
-          return (
-            <>
-              {mappedScriptModel && (
-                <script
-                  key={script.id}
-                  id={`_next-${script.id}-init`}
-                  data-nscript={computedStrategy}
-                  src={mappedScriptModel.urlScript.value.href}
-                  async={mappedScriptModel?.async?.value ? true : undefined}
-                  defer={mappedScriptModel?.defer?.value ? true : undefined}
-                />
-              )}
-            </>
-          );
-        }
-      })}
-    </>
-  );
-};
+const PageAnalyticsScripts = (props: AnalyticsScriptsProps): JSX.Element => (
+  <>
+    {props.scripts.map((script: AnalyticScriptItem) => {
+      if (script.template.id === TEMPLATES_ID.contentScript) {
+        const contentScriptModel = mapToNew<ContentScript>(script);
+        return (
+          <>
+            {contentScriptModel && (
+              <>
+                {!contentScriptModel.noScript.value && (
+                  <script
+                    key={script.id}
+                    id={`_next-${script.id}-init`}
+                    type="text/javascript"
+                    data-nscript={props.strategy}
+                    dangerouslySetInnerHTML={{ __html: contentScriptModel.contentScript.value }}
+                  />
+                )}
+                {contentScriptModel.noScript.value && (
+                  <noscript
+                    key={script.id}
+                    dangerouslySetInnerHTML={{ __html: contentScriptModel.contentScript.value }}
+                  />
+                )}
+              </>
+            )}
+          </>
+        );
+      } else {
+        const mappedScriptModel = mapToNew<UrlScript>(script);
+        return (
+          <>
+            {mappedScriptModel && (
+              <script
+                key={script.id}
+                id={`_next-${script.id}-init`}
+                data-nscript={props.strategy}
+                src={mappedScriptModel.urlScript.value.href}
+                async={mappedScriptModel?.async?.value ? true : undefined}
+                defer={mappedScriptModel?.defer?.value ? true : undefined}
+              />
+            )}
+          </>
+        );
+      }
+    })}
+  </>
+);
 
 export default PageAnalyticsScripts;
