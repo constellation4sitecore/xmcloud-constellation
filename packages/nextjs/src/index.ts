d4e5920d0942ab03598c21828d561efdd5b61a9f
Roberto Armas <roberto.armas@verndale.com>
2025-10-01 10:05:36 -0500

    performance optimization

--- packages/nextjs/src/index.ts
+++ packages/nextjs/src/index.ts
@@ -1,5 +1,4 @@
 export { default as debug, Debugger, enableDebug } from './debug';
 export * from './fields-extensions';
-export * from './graphql-client-factory';
 export * from './helpers';
 export * from './utils';
